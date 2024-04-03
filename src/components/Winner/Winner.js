696fc3600bc6d0999c4f584d23c116440b471441
davep80 <davidpaq1@gmail.com>
2024-04-03 16:36:42 -0400

    new error handling to sync changes in backend issue #5

--- src/components/Winner/Winner.js
+++ src/components/Winner/Winner.js
@@ -7,11 +7,12 @@ import { ballons } from "../../assets";
 
 const Winner = () => {
   const [winner, setWinner] = useState(null);
+  const [loading, setLoading] = useState(true);
   const { id } = useParams();
 
   async function getData() {
     const response = await getRafWinner(id);
-    if (response.ok) {
+    if (response?.ok) {
       const data = await response.json();
       setWinner(data.data);
     }
@@ -19,34 +20,42 @@ const Winner = () => {
 
   useEffect(() => {
     getData();
+    setLoading(false);
   }, []);
 
-  return (
-    <>
-      {winner?.winner_id ? (
-        <div>
-          <h1>Winner</h1>
-          <div className="card" style={{ width: "18rem" }}>
-            <img src={ballons} alt="celebration" />
-            <div className="card-body">
-              <h5 className="card-title">
-                {winner.first_name} {winner.last_name}
-              </h5>
-              <p className="card-text">raffle Id# {winner.id}</p>
-              <p className="card-text">
-                <FaEnvelope /> {winner.email}
-              </p>
-              <p className="card-text">
-                <FaPhone /> {winner.phone || ""}
-              </p>
+  if (loading) {
+    return (
+      <div className="spinner-border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    );
+  } else
+    return (
+      <>
+        {winner?.winner_id ? (
+          <div>
+            <h1>Winner</h1>
+            <div className="card" style={{ width: "18rem" }}>
+              <img src={ballons} alt="celebration" />
+              <div className="card-body">
+                <h5 className="card-title">
+                  {winner.first_name} {winner.last_name}
+                </h5>
+                <p className="card-text">raffle Id# {winner.id}</p>
+                <p className="card-text">
+                  <FaEnvelope /> {winner.email}
+                </p>
+                <p className="card-text">
+                  <FaPhone /> {winner.phone || ""}
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      ) : (
-        <WinnerForm getData={getData} />
-      )}
-    </>
-  );
+        ) : (
+          <WinnerForm getData={getData} />
+        )}
+      </>
+    );
 };
 
 export default Winner;
